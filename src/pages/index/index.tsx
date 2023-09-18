daa333430e77f4aa13e5a26a829385d2e3458d03
shunhe <1050794513@qq.com>
2023-09-18 13:30:27 +0800

    createIntersectionObserver失效

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,25 +4,78 @@ import "./index.css";
 import C1 from "../../components/C1";
 import C2 from "../../components/C2";
 import store from "../../store/counter";
+import { useEffect } from "react";
+import Taro, { PageInstance } from "@tarojs/taro";
 
 const Index = () => {
   console.log("store", store);
+  useEffect(() => {
+    let _observer: any;
+    //组件显示事件
+    const showCommonEvent = () => {
+      _observer = Taro.createIntersectionObserver(
+        Taro.getCurrentInstance().page as PageInstance,
+        {
+          thresholds: [0, 1],
+          observeAll: true,
+        }
+      );
+      const options = {};
+      setTimeout(() => {
+        _observer.relativeToViewport(options).observe(`#a1`, (res) => {
+          console.log("resresres", res.intersectionRatio);
+          if (res.intersectionRatio === 0) {
+            return;
+          }
+          if (res.intersectionRatio === 1) {
+            return;
+          }
+        });
+      }, 1000);
+    };
+    setTimeout(() => {
+      showCommonEvent();
+    });
+    //页面数据监听
+    return () => {
+      if (_observer) {
+        _observer.disconnect();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <View>
-      <Button onClick={store.increment}>+</Button>
-      <Button onClick={store.decrement}>-</Button>
-      <Button onClick={store.incrementAsync}>Add Async</Button>
-      <Text>{store.obj.counter}</Text>
-      <C1></C1>
-      <C2></C2>
-      <Button
-        onClick={() => {
-          store.obj = { ...store.obj };
+    <>
+      <View
+        style={{
+          height: "500px",
+          width: "500px",
+          background: "red",
         }}
-      >
-        克隆
-      </Button>
-    </View>
+      ></View>
+      <View
+        id="a1"
+        style={{
+          height: "300px",
+          width: "300px",
+          background: "green",
+        }}
+      ></View>
+      <View
+        style={{
+          height: "300px",
+          width: "300px",
+          background: "blue",
+        }}
+      ></View>
+      <View
+        style={{
+          height: "300px",
+          width: "300px",
+          background: "black",
+        }}
+      ></View>
+    </>
   );
 };
 export default observer(Index);
